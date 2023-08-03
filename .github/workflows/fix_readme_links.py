ace74b9f296a0c2bf467322cf5c818d076761d96
isgeekcode <ptuguitar@naver.com>
2023-08-03 12:53:06 +0900

    fix

--- .github/workflows/fix_readme_links.py
+++ .github/workflows/fix_readme_links.py
@@ -4,19 +4,24 @@
 # 아래는 예시 코드입니다. README 파일을 수정하는 작업을 여기에 추가하세요.
 
 # 예시: README 파일의 링크들을 소문자로 변경
+import re
+
 def fix_readme_links(readme_content):
-    import re
     fixed_content = re.sub(r'\[(.*?)\]\(#(.*?)\)', lambda x: f"[{x.group(1)}](#{x.group(2).lower()})", readme_content)
     return fixed_content
 
-# README 파일의 내용을 읽어옴
-with open("README.md", "r") as file:
-    readme_content = file.read()
+try:
+    # README 파일의 내용을 읽어옴
+    with open("README.md", "r") as file:
+        readme_content = file.read()
 
-# 링크 수정
-fixed_content = fix_readme_links(readme_content)
+    # 링크 수정
+    fixed_content = fix_readme_links(readme_content)
 
-# 수정된 내용을 README 파일에 다시 씀
-with open("README.md", "w") as file:
-    file.write(fixed_content)
+    # 수정된 내용을 README 파일에 다시 씀
+    with open("README.md", "w") as file:
+        file.write(fixed_content)
 
+    print("README 파일이 성공적으로 수정되었습니다.")
+except Exception as e:
+    print("스크립트 실행 중 오류가 발생했습니다:", e)
